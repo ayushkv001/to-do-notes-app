cd2de82591592d30af3c6e684a0390ae1e962b65
Ayush Kumar <ayushmrityunjay@gmail.com>
2022-07-30 23:22:52 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import trash from './trash.svg'
 import './App.css';
 
 function App() {
+  const [item,setItem] = useState("");
+  const [list,setList] = useState([])
+
+
+  const addItem = (e)=>{
+    setList((prev)=>{
+      return [...prev,item];
+    })
+    setItem("")
+
+  }
+  const deleteItem = (id)=>{
+    setList((prev)=>{
+      return prev.filter((element,index)=>{
+        return id!==index;
+      })
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='div1'>
+        <br/>
+        <h1>To-Do List</h1>
+        <br/>
+        <input type="text" placeholder='Add Item' value={item} onChange={(e)=>{setItem(e.target.value)}}/>
+        <button className='but' onClick={addItem}>+</button>
+
+        <ul >
+          {
+            list.map((items,id)=>{
+              return <li><div className='items'><div className='index'>{id+1}</div><div className='item'>{items}</div><div className='image'><button onClick={()=>{deleteItem(id)}}><img src={trash} /></button></div></div></li>
+            })
+          }
+        </ul>
+      </div>
     </div>
   );
 }
